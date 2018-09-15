432db5b19e4af3652d4082b2744419667c1685fb
marlapatisunayana <marlapatisunayana@192.168.1.34>
2018-09-15 15:25:46 -0500

    server

---
+++ Client/src/com/client/Main.java
@@ -0,0 +1,39 @@
+package com.client;
+
+import java.io.IOException;
+import java.util.Scanner;
+import java.net.Socket;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		try(Socket socket = new Socket("localhost",5000)){
+			BufferedReader echoes = new BufferedReader(
+					new InputStreamReader(socket.getInputStream()));
+			PrintWriter stringToEcho = new PrintWriter(socket.getOutputStream(), true);
+
+            Scanner scanner = new Scanner(System.in);
+            String echoString;
+            String response;
+
+            do {
+                System.out.println("Enter string to be echoed: ");
+                echoString = scanner.nextLine();
+
+                stringToEcho.println(echoString);
+                if(!echoString.equals("exit")) {
+                    response = echoes.readLine();
+                    System.out.println(response);
+                }
+            } while(!echoString.equals("exit"));
+
+        } catch (IOException e) {
+            System.out.println("Client Error: " + e.getMessage());
+
+        }
+    }
+}
